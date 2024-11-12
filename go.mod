684e8923d36487fab346c1e4fae20c92834ccb94
asinyaev <asinyaev@icloud.com>
2024-11-12 23:40:11 +0300

    init project, use mymath module

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/asinyaev/test-go-project.git
+
+go 1.23.2
+
+require github.com/asinyaev/mymath v1.0.0 // indirect
---
+++ main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"fmt"
+	"github.com/asinyaev/mymath"
+)
+
+func main() {
+	fmt.Println(mymath.Add(2, 2))
+}
