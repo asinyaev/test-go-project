f3a125d0fc6f81c5e529bf8da45c60e8fb28e260
asinyaev <asinyaev@icloud.com>
2024-11-12 23:56:10 +0300

    add another major version

--- go.mod
+++ go.mod
@@ -2,4 +2,6 @@ module github.com/asinyaev/test-go-project.git
 
 go 1.23.2
 
-require github.com/asinyaev/mymath v1.0.1 // indirect
+require github.com/asinyaev/mymath v1.0.1
+
+require github.com/asinyaev/mymath/v2 v2.0.1
--- main.go
+++ main.go
@@ -3,9 +3,11 @@ package main
 import (
 	"fmt"
 	"github.com/asinyaev/mymath"
+	mymath2 "github.com/asinyaev/mymath/v2"
 )
 
 func main() {
 	fmt.Println(mymath.Add(2, 2))
 	fmt.Println(mymath.Sub(2, 2))
+	fmt.Println(mymath2.Add(1, 2, 3))
 }
